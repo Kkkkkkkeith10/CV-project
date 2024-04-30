350a5106361b9593ad85f20d957fc1252625ea35
Kkkkkkkeith10 <134905899+Kkkkkkkeith10@users.noreply.github.com>
2024-05-01 11:27:10 +1200

    added in finding white pixel

--- Project.py
+++ Project.py
@@ -1,7 +1,9 @@
 import cv2
+import numpy as np
 
-
-img = cv2.imread("WeldGapImages/Set 1/image0001.jpg")
+# img = cv2.imread("WeldGapImages/Set 3/image0300.jpg")
+img = cv2.imread("WeldGapImages/Set 2/image0200.jpg")
+# img = cv2.imread("WeldGapImages/Set 1/image0001.jpg")
 save_file = r'WeldGapImages/ProcessedSet1/image0001.jpg'
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 1)
@@ -11,6 +13,14 @@ t_lower = 120 # Lower Threshold
 t_upper = 255 # Upper threshold
 # Applying the Canny Edge filter
 edge = cv2.Canny(blurred_image, t_lower, t_upper,True)
+
+# Assuming 'edge' is your edge-detected image
+row_of_pixels = edge[70, :]
+# Find indices of white pixels (where pixel value is 255)
+x_coordinates = np.where(row_of_pixels >= 250)[0]
+print(x_coordinates)
+
+
 # Saving the image
 cv2.imwrite(save_file, edge)
 # cv2.imshow('Edge', edge)
