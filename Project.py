11ed9690523cbca3af41ed0e2f5d96f70fcb1ba9
Kkkkkkkeith10 <134905899+Kkkkkkkeith10@users.noreply.github.com>
2024-05-01 11:45:43 +1200

    fixed bug

--- Project.py
+++ Project.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("WeldGapImages/Set 3/image0300.jpg")
-# img = cv2.imread("WeldGapImages/Set 2/image0200.jpg")
+# img = cv2.imread("WeldGapImages/Set 3/image0300.jpg")
+img = cv2.imread("WeldGapImages/Set 2/image0200.jpg")
 # img = cv2.imread("WeldGapImages/Set 1/image0001.jpg")
 save_file = r'WeldGapImages/ProcessedSet1/image0001.jpg'
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -17,21 +17,21 @@ edge = cv2.Canny(blurred_image, t_lower, t_upper,True)
 # Assuming 'edge' is your edge-detected image
 row_of_pixels = edge[70, :]
 # Find indices of white pixels (where pixel value is 255)
-x_coordinates = np.where(row_of_pixels >= 250)[0][0]
+x_coordinates = np.where(row_of_pixels >= 250)[0]
 print(x_coordinates)
 
 if(len(x_coordinates) > 0):
     thickness = 5
 
     start_point = (0, 70)  # Starting point of line (x, y)
-    end_point = (x_coordinates-thickness, 70)  # Ending point of line (x, y)
+    end_point = (x_coordinates[0]-thickness, 70)  # Ending point of line (x, y)
 
 
     color = (0,255, 0)  
 
     cv2.line(img, start_point, end_point,  color, thickness)
 
-    start_point_2 = (x_coordinates+thickness, 70)
+    start_point_2 = (x_coordinates[0]+thickness, 70)
     end_point_2 = (img.shape[1], 70)
 
     cv2.line(img, start_point_2, end_point_2, color, thickness)
