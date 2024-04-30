d60748ca1b6c64d7d73bf81c2c72e29f68a82767
Kkkkkkkeith10 <134905899+Kkkkkkkeith10@users.noreply.github.com>
2024-05-01 11:18:27 +1200

    added grey scale

--- Project.py
+++ Project.py
@@ -1,12 +1,13 @@
 import cv2
 
 
-img = cv2.imread("WeldGapImages/Set 2/image0200.jpg")
+img = cv2.imread("WeldGapImages/Set 1/image0001.jpg")
 save_file = r'WeldGapImages/ProcessedSet1/image0001.jpg'
-blurred_image = cv2.GaussianBlur(img, (15, 15), 1)
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 1)
 # cv2.imshow(save_file,img)
 # Setting parameter values
-t_lower = 254 # Lower Threshold
+t_lower = 120 # Lower Threshold
 t_upper = 255 # Upper threshold
 # Applying the Canny Edge filter
 edge = cv2.Canny(blurred_image, t_lower, t_upper,True)
