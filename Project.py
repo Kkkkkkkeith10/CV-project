0a78c6a314d4f52c59b3195b051e0157b3d3e764
Kkkkkkkeith10 <134905899+Kkkkkkkeith10@users.noreply.github.com>
2024-05-01 10:53:42 +1200

    edge detection code demo

--- Project.py
+++ Project.py
@@ -1,7 +1,16 @@
-def Bob():
-    print("billy")
+import cv2
 
-def main():
-    Bob()
 
-main()
+img = cv2.imread("WeldGapImages/Set 2/image0200.jpg")
+save_file = r'WeldGapImages/ProcessedSet1/image0001.jpg'
+cv2.imshow(save_file,img)
+# Setting parameter values
+t_lower = 120 # Lower Threshold
+t_upper = 255 # Upper threshold
+# Applying the Canny Edge filter
+edge = cv2.Canny(img, t_lower, t_upper)
+# Saving the image
+cv2.imwrite(save_file, edge)
+cv2.imshow('Edge', edge)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
