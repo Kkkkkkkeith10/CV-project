ccfc3d54251407c212ec384d847e52e9bfc36afa
Kkkkkkkeith10 <134905899+Kkkkkkkeith10@users.noreply.github.com>
2024-05-01 12:11:40 +1200

    add green line

--- Project.py
+++ Project.py
@@ -1,7 +1,9 @@
 import cv2
 import numpy as np
 
-# img = cv2.imread("WeldGapImages/Set 3/image0300.jpg")
+y = 70
+
+# img = cv2.imread("WeldGapImages/Set 3/image0307.jpg")
 img = cv2.imread("WeldGapImages/Set 2/image0200.jpg")
 # img = cv2.imread("WeldGapImages/Set 1/image0001.jpg")
 save_file = r'WeldGapImages/ProcessedSet1/image0001.jpg'
@@ -9,30 +11,35 @@ gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 1)
 # cv2.imshow(save_file,img)
 # Setting parameter values
-t_lower = 120 # Lower Threshold
+t_lower = 100 # Lower Threshold
 t_upper = 255 # Upper threshold
 # Applying the Canny Edge filter
-edge = cv2.Canny(blurred_image, t_lower, t_upper,True)
+edge = cv2.Canny(blurred_image, t_lower, t_upper)
 
 # Assuming 'edge' is your edge-detected image
-row_of_pixels = edge[70, :]
+row_of_pixels = edge[y, :]
 # Find indices of white pixels (where pixel value is 255)
-x_coordinates = np.where(row_of_pixels >= 250)[0]
-print(x_coordinates)
+x_coordinates = np.where(row_of_pixels >= 120)[0]
+
+
+
+
 
 if(len(x_coordinates) > 0):
+    x_coordinate = int(np.mean(x_coordinates))
+    print(x_coordinate)
     thickness = 5
 
-    start_point = (0, 70)  # Starting point of line (x, y)
-    end_point = (x_coordinates[0]-thickness, 70)  # Ending point of line (x, y)
+    start_point = (0, y)  # Starting point of line (x, y)
+    end_point = (x_coordinate-thickness, y)  # Ending point of line (x, y)
 
 
-    color = (0,255, 0)  
+    color = (0,255, 0)
 
     cv2.line(img, start_point, end_point,  color, thickness)
 
-    start_point_2 = (x_coordinates[0]+thickness, 70)
-    end_point_2 = (img.shape[1], 70)
+    start_point_2 = (x_coordinate+thickness, y)
+    end_point_2 = (img.shape[1], y)
 
     cv2.line(img, start_point_2, end_point_2, color, thickness)
 
